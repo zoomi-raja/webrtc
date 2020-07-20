781c32328e8e7103fd2e8d5f7d9a167701f499d8
zoomi-raja <zoomi.raja@gmail.com>
2020-07-20 21:57:31 +0500

    gitignore

---
+++ app/websocket/actions.js
@@ -0,0 +1,86 @@
+const { v4: uuidv4 } = require("uuid");
+const { sendTo, sendToAll } = require("../utility/helper");
+module.exports = (userMap, request, allConnections, currentConnection) => {
+	return {
+		login: () => {
+			let { name } = request;
+			if (userMap.has(name)) {
+				sendTo(currentConnection, {
+					type: "login",
+					success: false,
+					message: "Username is unavailable",
+				});
+			} else {
+				const id = uuidv4();
+				let loggedIn = [];
+				for (let { id, name: userName } of userMap.values()) {
+					loggedIn.push({ id, userName });
+				}
+				userMap.set(name, currentConnection);
+				currentConnection.name = name;
+				currentConnection.id = id;
+				sendTo(currentConnection, {
+					type: "login",
+					success: true,
+					users: loggedIn,
+				});
+				sendToAll(allConnections, "updateUsers", currentConnection);
+			}
+		},
+		offer: () => {
+			let { offer = "join me", name } = request;
+			const offerRecipient = userMap.get(name);
+			if (!!offerRecipient) {
+				sendTo(offerRecipient, {
+					type: "offer",
+					offer,
+					name: currentConnection.name,
+				});
+				sendTo(currentConnection, {
+					type: "offerSent",
+					offer,
+					name: offerRecipient.name,
+				});
+			} else {
+				sendTo(currentConnection, {
+					type: "error",
+					message: `User ${name} does not exist!`,
+				});
+			}
+		},
+		answer: () => {
+			const { name, answer = "ok" } = request;
+			const answerRecipient = userMap.get(name);
+			if (!!answerRecipient) {
+				sendTo(answerRecipient, {
+					type: "answer",
+					answer,
+				});
+				sendTo(currentConnection, {
+					type: "answerSent",
+					answer,
+				});
+			} else {
+				sendTo(currentConnection, {
+					type: "error",
+					message: `User ${name} does not exist!`,
+				});
+			}
+		},
+		candidate: () => {
+			const { name, candidate = "" } = request;
+			const candidateRecipient = userMap.get(name);
+			if (!!candidateRecipient) {
+				sendTo(candidateRecipient, {
+					type: "candidate",
+					candidate,
+				});
+			} else {
+				sendTo(currentConnection, {
+					type: "error",
+					message: `User ${name} does not exist!`,
+				});
+			}
+		},
+	};
+};
